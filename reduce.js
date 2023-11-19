3aa455a478ecf3d9a453dc0ad273a98282051e68
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2023-11-19 18:11:44 +0000

    reduce function

--- reduce.js
+++ reduce.js
@@ -6,7 +6,15 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) =>
+                      {
+                        console.log("Accumulator: " + acc, "Current Value: " + curr, "Total Value: ", acc + curr);
+                        return acc + curr;
+                      },0);
+console.log(sum); // Prints: 10
+    
+    
 
 const teamMembers = [
   {
@@ -32,6 +40,61 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience); // Prints: 16
 
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}
+, {});
+
+console.log(experienceByProfession);
+
+//Challenge 1
+
+
+let students = [
+  {
+    name: 'John',
+    subjects: ['maths', 'english', 'cad'],
+    teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+    results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+    name: 'Emily',
+    subjects: ['science', 'english', 'art'],
+    teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+    results: {science: 93, english: 73, art: 95},
+  },
+  {
+    name: 'Adam',
+    subjects: ['science', 'maths', 'art'],
+    teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+    results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+    name: 'Fran',
+    subjects: ['science', 'english', 'art'],
+    teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+    results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = students.reduce((acc, cur) =>
+                                {
+                                  acc = acc.max > cur.results.english ? acc : {
+                                    name: cur.name,
+                                    max: cur.results.english
+                                  };
+                                  return acc;
+                                }, {name: '', max: 0});
+console.log(biggest);
